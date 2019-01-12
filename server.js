1c09a49c2f73c65b9fbf95da00373f236e8f9c62
Daniel Truong <ddtruong06@gmail.com>
2019-01-12 03:15:47 -0800

    set up & initialized firebase

--- server.js
+++ server.js
@@ -14,10 +14,27 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 const twilio = require('twilio');
 const client = new twilio(config.twilioSid, config.twilioToken);
 
-client.messages.create({
-    body: 'hello world',
-    to: config.receiverNumber,  // Text this number
-    from: config.senderNumber // From a valid Twilio number
-})
-.then((message) => console.log(message.sid))
-.done();
+// client.messages.create({
+//     body: 'hello world',
+//     to: config.receiverNumber,  // Text this number
+//     from: config.senderNumber // From a valid Twilio number
+// })
+// .then((message) => console.log(message.sid))
+// .done();
+
+/* firebase stuff */
+const firebase = require("firebase");
+const fbSettings = {
+  apiKey: config.firebaseKey,
+  authDomain: config.firebaseAuth,
+  databaseURL:config.firebaseDB,
+  storageBucket: config.firebaseSB,
+};
+firebase.initializeApp(fbSettings);
+
+const databaseRef = firebase.database().ref();
+const storageRef = firebase.storage().ref();
+
+
+
+// store number & audio files
