33b768d9eb6c4b2028770459334bd96dc67a423a
Antony Nguyen <emilyhuongnguyen@gmail.com>
2019-01-12 02:40:36 -0800

    Add ejs setup

--- server.js
+++ server.js
@@ -3,21 +3,37 @@ global.config = require('./config.json');
 
 /* express stuff */
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+// Declare an instance of express
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser());
+app.use(cors());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Path joins the current directory name with views (aka current directory + views)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(port, function() {
+  console.log('we are gucci');
+});
 
 /* twilio stuff */
 const twilio = require('twilio');
 const client = new twilio(config.twilioSid, config.twilioToken);
-
+/*
 client.messages.create({
     body: 'hello world',
     to: config.receiverNumber,  // Text this number
     from: config.senderNumber // From a valid Twilio number
 })
 .then((message) => console.log(message.sid))
-.done();
+.done();*/
