3010c88080ac87b9158721ad2b48c103267f8b94
Daniel Truong <ddtruong06@gmail.com>
2019-01-13 00:40:55 -0800

    set up not rly working google speech-to-text & google translate

--- server.js
+++ server.js
@@ -28,6 +28,76 @@ app.get('/', function(request, response) {
 
 app.listen(port);
 
+
+
+/* Recording audio */
+
+//
+// const mic = require('mic');
+// const fs = require('fs');
+// const url = require('url');
+//
+// const micInstance = new mic({
+//   rate: '16000',
+//   channels: '1',
+//   debug: true,
+//   exitOnSilence: 6,
+//   fileType: 'mp3'
+// });
+//
+// const micInputStream = micInstance.getAudioStream();
+// const outputFileStream = fs.createWriteStream('test.mp3', {encoding: "binary"});
+//
+// micInputStream.pipe(outputFileStream);
+//
+// micInputStream.on('data', function(data) {
+//   console.log("Received Input Stream: " + data.length);
+// });
+// micInputStream.on('error', function(err){
+//   console.log("Error in Input Stream: " + err);
+// })
+//
+// micInputStream.on('startComplete', function(){
+//   console.log("Got SIGNAL startComplete");
+//   setTimeout(function() {
+//     micInstance.pause();
+//   }, 5000);
+//   setTimeout(function() {
+//     micInstance.stop();
+//   }, 5000);
+// });
+//
+// micInputStream.on('stopComplete', function() {
+//     console.log("Got SIGNAL stopComplete");
+//     outputFileStream.end();
+// });
+//
+// micInputStream.on('pauseComplete', function() {
+//     console.log("Got SIGNAL pauseComplete");
+//     setTimeout(function() {
+//         micInstance.resume();
+//     }, 5000);
+// });
+//
+// micInputStream.on('resumeComplete', function() {
+//     console.log("Got SIGNAL resumeComplete");
+//     setTimeout(function() {
+//         micInstance.stop();
+//     }, 5000);
+// });
+//
+// micInputStream.on('silence', function() {
+//     console.log("Got SIGNAL silence");
+// });
+//
+// micInputStream.on('processExitComplete', function() {
+//     console.log("Got SIGNAL processExitComplete");
+// });
+
+// micInstance.start();
+
+
+
 /* firebase stuff */
 const firebase = require("firebase");
 
@@ -73,7 +143,7 @@ databaseRef.once('value')
   });
 
 const bucketName = 'sb-hacks-19-videos';
-const filename = 'fakelink1.mp4';
+let filename = 'testlecture.mp3';
 const destFilename = `public/videos/${filename}`;
 const options = {
   // The path to which the file should be downloaded, e.g. "./file.txt"
@@ -88,7 +158,7 @@ async function download() {
     .download(options);
 }
 
-/*async function upload(){
+async function upload(){
   await storage.bucket(bucketName).upload(filename, function(err, file){
     if (!err) {
     console.log('your file is now in your bucket.');
@@ -96,7 +166,7 @@ async function download() {
     console.log('Error uploading file: ' + err);
   }
   });
-}*/
+}
 
 /* socket io */
 /*
@@ -111,4 +181,73 @@ io.on('connection', (socket) => {
   })
 })*/
 
-download();
+/* speech-to-text */
+async function main(){
+  const speech = require('@google-cloud/speech')  ;
+  const fs = require('fs');
+
+  const client = new speech.SpeechClient();
+  // name of file to transcribe
+  const fileName = 'public/videos/testAudio.mp3'
+
+  // reads local audio files
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+  const model = 'video'
+
+  const speechConfig = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+    // model: model
+  };
+
+  const audio = {
+    content: fs.readFileSync(fileName).toString('base64'),
+  };
+
+  const request = {
+    config: speechConfig,
+    audio: audio,
+  };
+
+  // Detects speech in the audio file
+  client
+    .recognize(request)
+    .then(data => {
+      console.log(data);
+      const response = data[0];
+      console.log(response);
+      const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+      console.log(`Transcription: `, transcription);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+}
+
+main();
+
+/* translation */
+// const {Translate} = require('@google-cloud/translate');
+// const projectId = config.projectID;
+// const translate = new Translate({
+//   projectId: projectId
+// });
+//
+// // text to translate
+// const text = 'Hello, world!';
+// // target language
+// const target = 'ru';
+
+// Translates the text into the target language. "text" can be a string for
+// translating a single piece of text, or an array of strings for translating
+// multiple texts.
+// let [translations] = await translate.translate(text, target);
+// translations = Array.isArray(translations) ? translations : [translations];
+// console.log('Translations:');
+// translations.forEach((translation, i) => {
+//   console.log(`${text[i]} => (${target}) ${translation}`);
+// });
